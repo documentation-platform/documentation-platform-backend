5a82e707acd51c24bcc7e8ceff62844c2cd9ecf0
Callum Brezden <callumbrezden@gmail.com>
2024-10-28 18:14:53 -0700

    CORS Setup

--- src/main/java/com/org/project/controller/UserController.java
+++ src/main/java/com/org/project/controller/UserController.java
@@ -1,19 +1,39 @@
 package com.org.project.controller;
 
+import com.org.project.model.User;
 import com.org.project.security.Secured;
+import com.org.project.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @Secured
     @GetMapping("/info")
-    public ResponseEntity<String> userInfo(HttpServletRequest request) {
+    public ResponseEntity<Map<String, Object>> userInfo(HttpServletRequest request) {
         String user_id = (String) request.getAttribute("user_id");
-        return new ResponseEntity<String>("User ID: " + user_id, HttpStatus.OK);
+
+        User user = userService.getUserFromId(user_id);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("user_id", user.getId());
+        responseBody.put("name", user.getName());
+        responseBody.put("email", user.getEmail());
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 }
